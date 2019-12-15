82795fb79928cab060575c776df29b0c970a2187
klitscher <kylelitscher@gmail.com>
2019-12-15 02:37:18 +0000

    Adding answer file for day 2 part 2

---
+++ day_2_pt2.py
@@ -0,0 +1,33 @@
+"""Module for reading opcodes"""
+
+def opcodes(path):
+    """method to read opcodes from a file
+    path: path to the file to be read
+    """
+
+    for first in range(99):
+        for second in range(99):
+            with open(path, 'r') as fp:
+                line = fp.readline()
+                ops = [int(x) for x in line.split(',') if x != '']
+            ops[1] = first
+            ops[2] = second
+            for i in range(0, len(ops), 4):
+                if ops[i] == 99:
+                    break
+                idx1 = ops[i + 1]
+                idx2 = ops[i + 2]
+                idx3 = ops[i + 3]
+                if ops[i] == 1:
+                    ops[idx3] = ops[idx1] + ops[idx2]
+                elif ops[i] == 2:
+                    try:
+                        ops[idx3] = ops[idx1] * ops[idx2]
+                    except IndexError:
+                        flag = False
+                        break
+                else:
+                    print('Something went wrong')
+            if ops[0] == 19690720:
+                return 100 * ops[1] + ops[2]
+    print("Never found the answer")
